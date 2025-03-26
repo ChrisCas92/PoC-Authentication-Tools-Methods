432e9791184b9a46018f78badc57ecff35531845
ChrisCas92 <chriscas.dev@gmail.com>
2025-03-26 19:40:41 +0100

    Changed Directory structure and updated Dependencies

---
+++ WindowsAuth_Kerberos+LDAP/jeebackend/src/main/java/com/windows_kerberos/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.windows_kerberos.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+  // resources will be automatically discovered
+}
